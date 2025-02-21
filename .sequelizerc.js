232cf4edaaaeb4fa82cdb2e0d92da286235f6d31
JuanHPassos <juan12.henriques@gmail.com>
2025-02-21 14:22:18 -0300

    Add servidor, junto com o ambiente ja preparado e com os arquivos organizados e criados.

---
+++ .sequelizerc.js
@@ -0,0 +1,9 @@
+// specify various options to pass as arguments to the CLI
+const path = require('path');
+
+module.exports = {
+    'config': path.resolve('./src/config', 'config.json'),
+    'models-path': path.resolve('./src/models'),
+    'seeders-path': path.resolve('./src/seeders'),
+    'migrations-path': path.resolve('./src/migrations')
+}
