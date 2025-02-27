a4bba5f3d46b1b60d46228db6029cc14c795bfe2
JuanHPassos <juan12.henriques@gmail.com>
2025-02-27 18:59:10 -0300

    Adicionado rotas das outras classes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 // Ponto de acesso para todas as rotas
 const express = require('express');
-const pessoas = require('./pessoasRoutes.js');
+const pessoas = require('./pessoasRoute.js');
+const categorias = require('./categoriasRoute.js');
+const cursos = require('./cursosRoute.js');
 
 module.exports = app => {
   // Recebe middlewares.
@@ -8,5 +10,7 @@ module.exports = app => {
     // converte para JSON os dados que vem como string.
     express.json(), 
     pessoas, // Todas rotas de pessoas.
+    categorias,
+    cursos
   );
 };
