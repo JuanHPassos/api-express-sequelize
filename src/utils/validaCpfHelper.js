47c16a33bfa6f89b39221b7a003cbe8a2be4a6fe
JuanHPassos <juan12.henriques@gmail.com>
2025-03-01 18:40:17 -0300

    Ajuste função de verificação de cpf

--- src/utils/validaCpfHelper.js
+++ src/utils/validaCpfHelper.js
@@ -1,3 +1,31 @@
 module.exports = (cpf) => {
-  return (cpf.length !== 11) ? false : true;
+  // Verifica se é string, o tamanho e se só possui letras/numeros.
+  if (typeof cpf !== 'string' || cpf.length !== 11 || !/^\d+$/.test(cpf)) {
+    return false;
+  }
+
+  // Verifica se todos os números são iguais (ex: "11111111111")
+  if (/^(\d)\1{10}$/.test(cpf)) {
+    return false;
+  }
+
+  let verificacao = 0;
+  // Calcula soma para verificação do primeiro digito
+  for(let i = 10; i >= 2; i--) {
+    verificacao += Number(cpf[10 - i]) * i;
+  }
+  
+  // Primeira verificação
+  if(((verificacao*10)%11)%10 !== Number(cpf[9])) return false;
+  
+  verificacao = 0;
+  // Calcula soma para verificação do segundo digito
+  for(let i = 11; i >= 2; i--) {
+    verificacao += Number(cpf[11 - i]) * i;
+  }
+
+  // Segunda verificação
+  if(((verificacao*10)%11)%10 !== Number(cpf[10])) return false;
+  
+  return true;
 };
