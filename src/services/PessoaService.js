6323eebdf495346b902f669e817a784d78c93e8f
JuanHPassos <juan12.henriques@gmail.com>
2025-03-02 19:35:21 -0300

    Adiciona transações do sequelize, para que caso haja um erro, o banco volte ao estado anterior.

--- src/services/PessoaService.js
+++ src/services/PessoaService.js
@@ -1,3 +1,4 @@
+const dataSource = require('../database/models');
 const Services = require('./Services.js');
 
 class PessoaServices extends Services {
@@ -25,11 +26,12 @@ class PessoaServices extends Services {
   }
 
   async cancelaPessoaEMatriculas (estudanteId){
-    await super.atualizaRegistro({ ativo: false }, { id: estudanteId });
-    // Alteração na tabela de matrículas
-    await this.matriculaServices.atualizaRegistro({status: 'cancelado'}, { estudante_id: estudanteId });
+    return dataSource.sequelize.transaction(async (transacao) => {
+      await super.atualizaRegistro({ ativo: false }, { id: estudanteId }, transacao);
+      // Alteração na tabela de matrículas
+      await this.matriculaServices.atualizaRegistro({status: 'cancelado'}, { estudante_id: estudanteId }, transacao);
+    });
   }
-
 }
 
 module.exports = PessoaServices;
